382ddeaba1bedc606495b8644cd46c425908cf09
aiwithfarah <aiwithfarah@gmail.com>
2025-11-17 19:00:41 +0530

    Add files via upload

---
+++ Comparison Operators/logical_operators.py
@@ -0,0 +1,11 @@
+# used to chain conditionals
+
+# Logical AND - both conditions need to evaluate to true
+print(1 < 2 and 2 < 3)  # True
+
+# Logical OR - Atleast one cndition needs to evaluate to True
+print(1 < 2 or 2 > 3)  # true
+
+# NOT reversed the conditoon
+print(not True)  # False
+print(not 2 == 2)  # False
