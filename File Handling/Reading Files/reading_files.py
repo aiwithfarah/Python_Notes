d2012eaeead5374fb1282bf2118528672e3e98e2
Farah <aiwithfarah@gmail.com>
2025-11-24 14:24:40 +0530

    Add files via upload

--- File Handling/Reading Files/reading_files.py
+++ File Handling/Reading Files/reading_files.py
@@ -1,10 +1,15 @@
 # python reading files (txt, csv, json)
 
-file_path = "File Handling/output.txt"
+file_path = "File Handling/Writing Files/output.txt"
 
 try:
     with open(file_path, "r") as file:
+        single_line = file.readline()  # Sam Bam Tam
+        first_ten = file.read(10)  # Sam Bam Ta
         content = file.read()
+        print(single_line)
+        print(first_ten)
+        print(type(single_line))  # <class 'str'>
         print(content)
 except FileNotFoundError:
     print("File Not Found")
