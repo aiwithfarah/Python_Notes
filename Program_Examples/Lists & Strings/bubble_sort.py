16d4bdabdc8fafe070b0c23d19b28545fddc4aaa
Farah <aiwithfarah@gmail.com>
2025-12-13 15:36:12 +0530

    Add files via upload

---
+++ Program_Examples/Lists & Strings/bubble_sort.py
@@ -0,0 +1,18 @@
+my_list = []
+swapped = True
+num = int(input("How many elements do you want to sort?: "))
+
+for i in range(num):
+    val = int(input("Enter a list of numbers: "))
+    my_list.append(val)
+
+# print(my_list)
+while swapped:
+    swapped = False
+    for i in range(len(my_list)-1):
+        if my_list[i] > my_list[i+1]:
+            swapped = True
+            my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+
+
+print(my_list)
