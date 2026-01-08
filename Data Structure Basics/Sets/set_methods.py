8e2444b362ac1d6101018be754961ecff576222f
Farah <aiwithfarah@gmail.com>
2026-01-08 16:44:54 +0530

    Update set_methods.py

--- Data Structure Basics/Sets/set_methods.py
+++ Data Structure Basics/Sets/set_methods.py
@@ -3,3 +3,14 @@ fruits.remove(1)
 print(fruits)  # {2, 3, 4, 5, 6}
 fruits.add(44)
 print(fruits)  # {2, 3, 4, 5, 6, 44}
+
+fruits.discard(4)
+print(fruits)  # {2, 3, 5, 6, 44}
+
+# Removes an item safely (No error if the item is missing).
+fruits.discard("Apple")
+print(fruits)  # {2, 3, 5, 6, 44}
+
+# .pop(): Removes a random item (since sets are unordered).
+fruits.pop()
+print(fruits)  # {3, 5, 6, 44}
