c6e5bc640009cc9f2a225c54453d51e5982072b6
Farah <aiwithfarah@gmail.com>
2026-01-13 18:24:00 +0530

    Update sets.py

--- Data Structure Basics/Sets/sets.py
+++ Data Structure Basics/Sets/sets.py
@@ -1,3 +1,9 @@
+Sets require their items to be "hashable" (immutable). Because a List is mutable (you can change it later),
+Python forbids putting a List inside a Set. If you try { [1,2] }, Python throws a TypeError: unhashable type: 'list'.
+Fix: You can put a Tuple inside a Set, because Tuples are immutable.
+
+
+
 my_set = set()
 print(my_set)
 
@@ -5,3 +11,4 @@ my_set.add(1)
 print(my_set)  # {1}
 
 print(set([1, 1, 2, 3]))
+
